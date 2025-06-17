6992899c1dc680247c38902789997b879b7463a5
russellxz <162611099+russellxz@users.noreply.github.com>
2025-06-17 04:19:07 -0400

    Update help.js

--- plugins2/help.js
+++ plugins2/help.js
@@ -68,6 +68,7 @@ const handler = async (msg, { conn }) => {
 ⟢ ${usedPrefix}tagall / ${usedPrefix}invocar / ${usedPrefix}todos
 ⟢ ${usedPrefix}infogrupo
 ⟢ ${usedPrefix}damelink
+⟢ ${usedPrefix}antidelete on o off
 
 〔 Comandos De Juegos 〕
 ⟢ ${usedPrefix}verdad
@@ -78,6 +79,7 @@ const handler = async (msg, { conn }) => {
 
 〔 Configuración & Dueño 〕
 
+▣ ${usedPrefix}antideletepri on o off
 ▣ ${usedPrefix}setprefix ↷
   Cambiar prefijo del subbot
 ▣ ${usedPrefix}creador ↷
