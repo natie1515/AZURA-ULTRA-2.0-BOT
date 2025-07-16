24b6ad7438409d2d79a199d699e5fc3f4eaa4700
ᬊ✓•WorkCwp;Ofcᬊ𒈸 <122848691+WorkCwp@users.noreply.github.com>
2025-07-16 10:26:51 -0500

    ✓•WorkCwp;Ofcᬊ

--- plugins2/owner-update.js
+++ plugins2/owner-update.js
@@ -1,29 +1,11 @@
-import { exec } from 'child_process';
-
-let handler = async (m, { conn }) => {
-  m.reply(`Actualizando...`);
-
-  exec('git pull', (err, stdout, stderr) => {
-    if (err) {
-      conn.reply(m.chat, `Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
-      return;
-    }
-
-    if (stderr) {
-      console.warn('Advertencia durante la actualización:', stderr);
-    }
-
-    if (stdout.includes('Already up to date.')) {
-      conn.reply(m.chat, `bot actualizado.`, m);
-    } else {
-      conn.reply(m.chat, `Actualización realizada con éxito.\n\n${stdout}`, m);
-    }
-  });
-};
-
-handler.help = ['update'];
-handler.tags = ['owner'];
-handler.command = ['update'];
-handler.rowner = true;
-
-export default handler;
+import { execSync } from 'child_process'
+let handler = async (m, { conn, text }) => {
+if (global.conn.user.jid == conn.user.jid) {
+let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+conn.reply(m.chat, stdout.toString(), m)
+}}
+handler.help = ['update']
+handler.tags = ['owner']
+handler.command = /^update|actualizar$/i
+handler.rowner = true
+export default handler
